5fb81605d08c70c3f87ed77f15c5c3a45772bbaf
Alex Evans <aaevans03@gmail.com>
2024-11-06 12:04:06 -0700

    Added login screen

--- src/app.jsx
+++ src/app.jsx
@@ -23,7 +23,7 @@ export default function App() {
                         <menu className="navbar-nav">
                             <li className="nav-item">
                                 <NavLink className="nav-link active" to="">
-                                    {/* Maybe get rid of this active link coloring? */}
+                                    {/* Maybe get rid of this active link coloring? Or can I add a function to change it? */}
                                     Home
                                 </NavLink>
                             </li>
