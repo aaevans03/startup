b95ce2ba123c736b14690fce33ce8fa884bc4310
Alex Evans <aaevans03@gmail.com>
2024-11-05 22:20:06 -0700

    New app.jsx file, preparing for React

--- index.jsx
+++ index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './src/app';
 
+// Get the element with id root from index.html, insert our app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './app.css';
+
+export default function App() {
+    return (
+        <>
+            <main>
+                <h1>App will display here</h1>
+            </main>
+        </>
+    );
+}
