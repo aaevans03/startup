200c539bbdee302695e6bd4e33a571758c4175bf
Alex Evans <aaevans03@gmail.com>
2024-11-07 13:38:38 -0700

    Deleted original HTML files, added framework for future web services on about page

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import "./about.css"
 
 export function About() {
+
+    // get ready for the calls to an external web service for the image and quote
+    const [imageUrl, setImageUrl] = React.useState("");
+    const [quote, setQuote] = React.useState("loading...");
+    const [quoteAuthor, setQuoteAuthor] = React.useState("unknown");
+
+    React.useEffect(() => {
+        setImageUrl("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExODdudjlzZWN3dTFidmE5am5laHZibDcydXRwbHRzdnRkcmlpc2R6MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/bCvO8biVh8WyI/giphy.webp");
+        setQuote("Ni de mama hen pang");
+        setQuoteAuthor("every Chinese speaker ever");
+    })
+    
+
     return (
         <main>
             <div className="about-page">
@@ -20,16 +33,16 @@ export function About() {
                     <div className="row g-0">
                     <div className="gif">
                         {/* <!-- In the future, a link to an external web service will be put here that grabs a random laundry gif from GIPHY --> */}
-                        <img height="100%" src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExODdudjlzZWN3dTFidmE5am5laHZibDcydXRwbHRzdnRkcmlpc2R6MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/bCvO8biVh8WyI/giphy.webp" alt="Random GIF" />
+                        <img height="100%" src={imageUrl} alt="Random GIF" />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
                             <h3 className="card-title">Inspiration zone</h3>
                             <p className="card-text"><small className="text-body-secondary">Feeling unmotivated to do your laundry? Come back here to find a random laundry GIF and quote that will make your day!</small></p>
                             <p className="card-text">
-                                <div className="quote-text">"Laundry today or naked tomorrow"</div>
+                                <div className="quote-text">"{quote}"</div>
                                 <small className="text-body-secondary">
-                                <div className="quote">~ A sign that used to be in our laundry room</div></small>
+                                <div className="quote">~ {quoteAuthor}</div></small>
                             </p>
                         </div>
                     </div>
